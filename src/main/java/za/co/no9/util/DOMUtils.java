db6bc060ca373d0d2ca0f32b632aa895c6296693
graeme.lockley <Password1>
2014-05-07 19:24:02 +0200

    Simplified and improved variable names

--- src/main/java/za/co/no9/util/DOMUtils.java
+++ src/main/java/za/co/no9/util/DOMUtils.java
@@ -4,7 +4,6 @@ import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
@@ -12,18 +11,19 @@ import java.io.IOException;
 
 public class DOMUtils {
 
-    public static Document create(String xmlContent) throws IOException, ParserConfigurationException, SAXException {
+    public static Document create(String xmlContent) throws SAXException {
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(xmlContent.getBytes())) {
-            org.xml.sax.InputSource vInputSource;
-            vInputSource = new org.xml.sax.InputSource(inputStream);
-            return openDocument(vInputSource);
+            InputSource inputSource = new InputSource(inputStream);
+            return openDocument(inputSource);
+        } catch (IOException | ParserConfigurationException ex) {
+            throw new SAXException(ex);
         }
     }
 
-    private static Document openDocument(InputSource aInputSource) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder vDocumentBuilder = dbFactory.newDocumentBuilder();
-
-        return vDocumentBuilder.parse(aInputSource);
+    private static Document openDocument(InputSource inputSource) throws ParserConfigurationException, IOException, SAXException {
+        return DocumentBuilderFactory
+                .newInstance()
+                .newDocumentBuilder()
+                .parse(inputSource);
     }
 }
